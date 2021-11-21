e1aa0ebb22a3288eecdf918e689fa075f5b1af2b
StrictName <62078976+StrictName@users.noreply.github.com>
2021-11-21 13:48:31 -0600

    Add files via upload

--- nurseLog-LoginBackend/public/back/datos.php
+++ nurseLog-LoginBackend/public/back/datos.php
@@ -17,21 +17,21 @@ include('conexionDB.php');
     $password=$_POST['password'];
     $cpassword=$_POST['cpassword'];
 
-	
-	$consulta = "INSERT INTO usuarios (enfermera_id, contrasena, nombres, apellidos, departamento, turno) VALUES ('$id', '$password', '$firstName','$lastName','$department','$shift')";
-	if (hacerQuery($consulta)) {
-        echo "Su cuenta ha sido registrada correctamente<br>";
-	} else {
-		echo "Error al guardar los datos<br>";
+	if ($password == $cpassword) {
+        $consulta = "INSERT INTO usuarios (enfermera_id, contrasena, nombres, apellidos, departamento, turno) VALUES ('$id', '$password', '$firstName','$lastName','$department','$shift')";
+        if (hacerQuery($consulta)) {
+            echo "Su cuenta ha sido registrada correctamente<br>";
+        } else {
+            echo "Error al guardar los datos<br>";
+        }
+        echo "<br><br><form action='../../index.html' method='post'><button id='login' type='submit' class='btn'>Log in</button></form>";
+    } else {
+        echo "Las contraseñas proporcionadas no coinciden<br>";
+        echo "<br><br> <form action='../pages/register.html' method='post'> <button id='register' type='submit' class='btn'>Volver a intentarlo</button> </form>";
+
     }
 
 ?>
-
-    <br><br>
-    <form action="../../index.html" method="post">
-        <button id='login' type='submit' class='btn'>Log in</button>
-    </form>
-
 
 </body>
 </html>
